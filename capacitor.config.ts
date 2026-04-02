9eb834defe4a3538ea355a1ebe22b2662a31d61e
lexbasevisionares-cell <lexbasevisionares-cell@users.noreply.github.com>
2026-04-02 14:46:52 +0300

    Fix: Updated Bundle ID to com.lexbase.addictionticker

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.addictionticker.app',
+  appId: 'com.lexbase.addictionticker',
   appName: 'Addiction Ticker',
   webDir: 'dist'
 };
